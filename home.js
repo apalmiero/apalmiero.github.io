511799d5836571a22d4ff00f0f5e3e6e97785741
apalmiero21 <apalmiero21@gmail.com>
2025-12-31 04:47:38 -0500

    Added cv, projects page, contact me page, photo album page

--- home.js
+++ home.js
@@ -1,8 +1,23 @@
 var coll = document.getElementsByClassName("collapsible");
+var collProject = document.getElementsByClassName("collapsible-project");
 var i;
 
+// Handle regular collapsibles
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+// Handle project collapsibles
+for (i = 0; i < collProject.length; i++) {
+  collProject[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
